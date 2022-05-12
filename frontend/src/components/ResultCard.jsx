b9de22e749c3a80e210e6fdd24c64a7ce6c84ff6
kaitolight <alexandre.pillias@gmail.com>
2022-05-12 22:00:44 +0200

    started working on style

--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -4,32 +4,51 @@ import "../styles/ResultCard.css";
 export default function ResultCard({ pullResult }) {
   return (
     <div className="result-box">
-      Real-time air quality information : {pullResult.aqi}
-      <br />
-      Name of the monitoring station : {pullResult.city && pullResult.city.name}
-      <br />
-      Local measurement time : {pullResult.time && pullResult.time.s}
-      <br />
-      <br />
-      {pullResult.iaqi && pullResult.iaqi.pm25.v
-        ? `The current value of Fine particulate matter (PM2.5) is ${pullResult.iaqi.pm25.v} μg/m3`
-        : null}{" "}
-      <br />
-      {pullResult.iaqi && pullResult.iaqi.pm10.v
-        ? `The current value of Coarse particulate matter (PM10) is ${pullResult.iaqi.pm10.v} μg/m3`
-        : null}
-      <br />
-      {pullResult.iaqi && pullResult.iaqi.no2.v
-        ? `The current value of Nitrogen dioxide (PM10) is ${pullResult.iaqi.no2.v} μg/m3`
-        : null}
-      <br />
-      {pullResult.iaqi && pullResult.iaqi.so2.v
-        ? `The current value of Sulfur dioxide (PM10) is ${pullResult.iaqi.so2.v} μg/m3`
-        : null}
-      <br />
-      {pullResult.iaqi && pullResult.iaqi.o3.v
-        ? `The current value of Ozone (PM10) is ${pullResult.iaqi.o3.v} μg/m3`
-        : null}
+      <h1 className="result-title">
+        {pullResult.city && pullResult.city.name}
+      </h1>
+      <p>
+        The AQI (Air Quality Index) of the chosen city is{" "}
+        <span className="result-aqi">{pullResult.aqi}</span>
+      </p>
+      <p>Local measurement time : {pullResult.time && pullResult.time.s}</p>
+      <ul>
+        <li>
+          {pullResult.iaqi && pullResult.iaqi.pm25.v
+            ? `The current amount of Fine particulate matter (PM2.5) in ${
+                pullResult.city && pullResult.city.name
+              } is ${pullResult.iaqi.pm25.v} μg/m3`
+            : null}{" "}
+        </li>
+        <li>
+          {pullResult.iaqi && pullResult.iaqi.pm10.v
+            ? `The current amount of Coarse particulate matter (PM10) in ${
+                pullResult.city && pullResult.city.name
+              } is ${pullResult.iaqi.pm10.v} μg/m3`
+            : null}
+        </li>
+        <li>
+          {pullResult.iaqi && pullResult.iaqi.no2.v
+            ? `The current amount of Nitrogen dioxide (PM10) in ${
+                pullResult.city && pullResult.city.name
+              } is ${pullResult.iaqi.no2.v} μg/m3`
+            : null}
+        </li>
+        <li>
+          {pullResult.iaqi && pullResult.iaqi.so2.v
+            ? `The current amount of Sulfur dioxide (PM10) in ${
+                pullResult.city && pullResult.city.name
+              } is ${pullResult.iaqi.so2.v} μg/m3`
+            : null}
+        </li>
+        <li>
+          {pullResult.iaqi && pullResult.iaqi.o3.v
+            ? `The current amount of Ozone (PM10) in ${
+                pullResult.city && pullResult.city.name
+              } is ${pullResult.iaqi.o3.v} μg/m3`
+            : null}
+        </li>
+      </ul>
     </div>
   );
 }
